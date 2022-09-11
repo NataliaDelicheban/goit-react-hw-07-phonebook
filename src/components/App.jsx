6e34c17585a2e74cb1da6e419244e53356ba70d0
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-09-11 23:48:11 +0200

    update folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,16 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-
-import { addContact, removeContact } from 'redux/contacts/contacts-slice';
-import { setFilter } from 'redux/filter/filter-slice';
-import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
-import { getFilter } from 'redux/filter/filter-selectors';
-
-import {Filter} from './Filter/Filter';
-
-export function App() {
-  const contacts = useSelector(getFilteredContacts);
-  const filter = useSelector(getFilter);
-
-  const dispatch = useDispatch();
-
-  const onAddContact = (payload) => {
-    const action = addContact(payload);
-    dispatch(action)
-  }
-
-  const onRemoveContact = (payload) => {
-    dispatch(removeContact(payload));
-  }
-
-  const onSetFilter = ({target}) => {
-    dispatch(setFilter(target.value));
-  }
-
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={onAddContact}/>
-        <h2>Contacts</h2>
-        <Filter onChange={onSetFilter} value={filter} />
-        <ContactList contacts={contacts} removeContact={onRemoveContact} />
-          </>
-    );
-}
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
