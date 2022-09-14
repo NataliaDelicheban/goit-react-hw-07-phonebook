af220b443233015f62da3a128be7b7990dc30860
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-09-14 12:14:35 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,64 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
-
-import { addContact, removeContact } from 'redux/contacts/contacts-slice';
-import { setFilter } from 'redux/filter/filter-slice';
-import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
-import { getFilter } from 'redux/filter/filter-selectors';
-
 import {Filter} from './Filter/Filter';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 export function App() {
-  const contacts = useSelector(getFilteredContacts);
-  const filter = useSelector(getFilter);
+  const [contacts, setContacts] = useState(() => { return JSON.parse(localStorage.getItem('contacts')) ?? initialContacts });
+  const [filter, setFilter] = useState('');
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
-  const onAddContact = ({ name, number }) => {
-    const commonName = contacts.find(contact => {
-      return contact.name.toLowerCase() === name.toLowerCase();
-    });
-    if (commonName) {
+  const onSubmit = ({name, number}) => {
+    const isInContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    
+    if (isInContact) {
       alert(`${name} is already in contacts!`);
-
+      return;
     }
-    const action = addContact({name, number});
-    dispatch(action)
-  }
 
-  const onRemoveContact = (payload) => {
-    dispatch(removeContact(payload));
-  }
+    const newContact = { id: nanoid(), name, number };
+    setContacts([...contacts, newContact]);
+  };
 
-  const onSetFilter = ({target}) => {
-    dispatch(setFilter(target.value));
-  }
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  const changeFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  const getFilteredContacts = () => {
+    const normaslizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normaslizedFilter),
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
 
     return (
       <>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={onAddContact}/>
+        <ContactForm onSubmit={onSubmit} />
         <h2>Contacts</h2>
-        <Filter onChange={onSetFilter} value={filter} />
-        <ContactList contacts={contacts} removeContact={onRemoveContact} />
+        <Filter onChange={changeFilter} value={filter} />
+        <ContactList
+          contacts={filteredContacts}
+          onClick={deleteContact} />
           </>
     );
 }
